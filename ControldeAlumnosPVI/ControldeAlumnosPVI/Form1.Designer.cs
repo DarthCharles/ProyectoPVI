8530df3a77124082c778fb381b895942ecfa831f
Charles <carlos.darth@gmail.com>
2014-07-04 15:55:23 -0700

    tampoco

--- ControldeAlumnosPVI/ControldeAlumnosPVI/Form1.Designer.cs
+++ ControldeAlumnosPVI/ControldeAlumnosPVI/Form1.Designer.cs
@@ -356,7 +356,7 @@
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
