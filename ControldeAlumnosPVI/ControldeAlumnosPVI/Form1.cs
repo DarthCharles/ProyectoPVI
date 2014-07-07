46552f748185805ff88eed416a4461473a5481a1
Charles <carlos.darth@gmail.com>
2014-07-07 00:15:15 -0700

    cuando guardas te guardan todas las tablas

--- ControldeAlumnosPVI/ControldeAlumnosPVI/Form1.cs
+++ ControldeAlumnosPVI/ControldeAlumnosPVI/Form1.cs
@@ -552,6 +552,8 @@ namespace ControldeAlumnosPVI
     
                     guardarPuntos();
 
+                    MessageBox.Show("Datos guardados con éxito.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
                     refreshTables(ActiveGrupo().IdGrupo);
 
             }
@@ -580,7 +582,6 @@ namespace ControldeAlumnosPVI
                 }
             }
 
-            MessageBox.Show("Tareas guardadas con éxito.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         public void guardarTrabajos()
@@ -606,7 +607,6 @@ namespace ControldeAlumnosPVI
                 }
             }
 
-            MessageBox.Show("Trabajos guardadas con éxito.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         public void guardarExamenes()
@@ -631,7 +631,6 @@ namespace ControldeAlumnosPVI
                         "examen");
                 }
             }
-            MessageBox.Show("Examen guardadas con éxito.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         public void guardarLista()
@@ -643,7 +642,6 @@ namespace ControldeAlumnosPVI
                 con.tomarLista(listaAlumnos[i].IdAlumno, DateTime.Today.ToString("yyyy-MM-dd"), lista.Rows[i].Cells[3].Value.ToString());
             }
 
-            MessageBox.Show("Asistencias guardadas con éxito.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         public void guardarParticipaciones()
