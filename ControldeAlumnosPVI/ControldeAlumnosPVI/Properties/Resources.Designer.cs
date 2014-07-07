638ad3e0e9c722b3bdd142012656b4b43f9de66e
Charles <carlos.darth@gmail.com>
2014-07-06 18:02:00 -0700

    ya exporta a excel v2 :pencil2: dick

--- ControldeAlumnosPVI/ControldeAlumnosPVI/Properties/Resources.Designer.cs
+++ ControldeAlumnosPVI/ControldeAlumnosPVI/Properties/Resources.Designer.cs
@@ -173,6 +173,16 @@ namespace ControldeAlumnosPVI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ic_excel {
+            get {
+                object obj = ResourceManager.GetObject("ic_excel", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ic_group {
             get {
                 object obj = ResourceManager.GetObject("ic_group", resourceCulture);
